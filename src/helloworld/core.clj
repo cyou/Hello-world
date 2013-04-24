d917a6e84a948031afc30cacebc79b6ad484d7db
cyou <simida1984@163.com>
2013-04-24 09:04:33 +0800

    Update core.clj

--- src/helloworld/core.clj
+++ src/helloworld/core.clj
@@ -1,13 +1,13 @@
 (ns helloworld.core
   (:use compojure.core, ring.adapter.jetty)
   (:require [compojure.route :as route]))
-(defn install-m3o []
+(defn index []
   (doseq [a (range 0 100)] 
     (println "a"))
-  "this is down")
+  :done)
 
 (defroutes main-routes
-  (GET "/install" [] (install-m3o))
+  (GET "/index" [] (index))
   (route/not-found "<h1>no found</h1>"))
 
 (run-jetty main-routes {:port 8088})
